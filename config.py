c96669694911ede138fbfc525c4b8c67bcac9feb
lihaoABC <lihao0035@gmail.com>
2018-05-25 11:34:35 +0800

    抽取config配置信息到config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import redis
+
+
+# Config类
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    SECRET_KEY = 'TgmL5kH7QEhnStpDZcpvvo1ip+4JJ3ovnGV9QmEqJwo='
+
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/information_F'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis 配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    # flask_session配置信息
+    SESSION_TYPE = 'redis'              # 指定session保存到redis中
+    SESSION_USE_SIGNER = True           # 让cookie中的session——id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis实例
+    PERMANENT_SESSION_LIFETIME = 86400  # session有效期，单位是秒
--- manager.py
+++ manager.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 # 导入数据库拓展，并添加相关配置
 from flask_sqlalchemy import SQLAlchemy
 # 导入redis数据库拓展，添加相关配置
@@ -10,32 +10,9 @@ from flask_session import Session
 # flask_script与数据库迁移拓展
 from flask_script import Manager
 from flask_migrate import MigrateCommand, Migrate
-
+from config import Config
 
 app = Flask(__name__)
-
-
-# Config类
-class Config(object):
-    """工程配置信息"""
-    DEBUG = True
-    SECRET_KEY = 'TgmL5kH7QEhnStpDZcpvvo1ip+4JJ3ovnGV9QmEqJwo='
-
-    # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/information_F'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis 配置
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    # flask_session配置信息
-    SESSION_TYPE = 'redis'              # 指定session保存到redis中
-    SESSION_USE_SIGNER = True           # 让cookie中的session——id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session有效期，单位是秒
-
-
 
 
 app.config.from_object(Config)
@@ -53,6 +30,7 @@ manager.add_command('db', MigrateCommand)
 
 @app.route('/index')
 def index():
+    session['name'] = 'mike'
     return 'index'
 
 
